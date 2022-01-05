8da780ce59ca41d63210ed697e8866c1a7b8fc27
Nickleus Jimenez <Nickleus-J@live.com.ph>
2022-01-05 11:17:57 +0800

    Add project files.

---
+++ ChainedBordz.Entities/Node.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ChainedBordz.Entities
+{
+    public class Node
+    {
+        public object Data { get; set; }
+        public string Name { get; set; }
+        public string Hash { get; set; }
+        public Node? PreviousNode { get; set; }
+        public static Node CreateNode(object _data, string _name="", Node? prev = null)
+        {
+            return new Node(_data, _name, prev);
+        }
+        private Node(object _data,string _name,Node? prev=null)
+        {
+            Data = _data;
+            Name = _name;
+            Hash = CalculateHash();
+            PreviousNode = prev;
+        }
+        public string CalculateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{Name}-{Data}{DateTime.UtcNow}");
+            byte[] outputBytes = sha256.ComputeHash(inputBytes);
+
+            return Convert.ToBase64String(outputBytes);
+        }
+    }
+}
---
+++ ChainedBordz/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ChainedBordz.Entities;
+using System.Text.Json;
+
+namespace ChainedBordz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Node prevNode = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddKeyValueBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(TxtKey.Text.Length>0&& TxtKeyValue.Text.Length > 0)
+            {
+                Node n = Node.CreateNode(TxtKeyValue.Text, TxtKey.Text, prevNode);
+                prevNode = n;
+                Label lbl = new Label();
+                lbl.Content = n.Data;
+                blockPanel.Children.Add(lbl);
+            }
+            
+        }
+
+        private void SeeJsonMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Node? currentNode = prevNode;
+            IList<Node> nodes = new List<Node>();
+           
+            txtJson.Text = JsonSerializer.Serialize(prevNode);
+        }
+    }
+}
